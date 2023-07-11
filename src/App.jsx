59cb5b775da07e88e00f1a4fa5279dc131b64ec4
AJ <mahidlawonaljun@gmail.com>
2023-07-11 10:21:28 +0800

    Make Card resuable: make its data dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
 import './App.css';
+import zafares from './assets/zafares.png';
 
 function App() {
   return (
     <>
       <Navbar />
       {/* <Hero /> */}
-      <Card />
+      <Card
+        img={zafares}
+        imgAlt="swimmer"
+        rate="5.0"
+        count={6}
+        country="USA"
+        title="Life lessons with Katie Zaferes"
+        amount={136}
+      />
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,25 @@
 import './Card.css';
-import zafares from '../assets/zafares.png';
 import star from '../assets/star.png';
 
-const Card = () => {
+const Card = ({ img, imgAlt, rate, count, country, title, amount }) => {
   return (
     <section className="card">
-      <img src={zafares} className="card__img" alt="swimmer" />
+      <img src={img} className="card__img" alt={imgAlt} />
       <div className="card__content">
         <div className="card__rating">
           <img src={star} className="card__rating-img " alt="star" />
-          <span className="card__rating-rate">5.0</span>
-          <span className="card__rating-count card__rating--gray">(6)</span>
+          <span className="card__rating-rate">{rate}</span>
+          <span className="card__rating-count card__rating--gray">
+            ({count})
+          </span>
           <span className="card__rating-dot card__rating--gray"></span>
-          <span className="card__rating-county card__rating--gray">USA</span>
+          <span className="card__rating-county card__rating--gray">
+            {country}
+          </span>
         </div>
-        <h3 className="card__title">Life lessons with Katie Zaferes</h3>
+        <h3 className="card__title">{title}</h3>
         <p className="card__price">
-          <strong>From $136</strong> / person
+          <strong>From ${amount}</strong> / person
         </p>
       </div>
     </section>
